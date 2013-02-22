4e3ea736b3cc06bcb8feedcae7b1f030707dc06d
RobIII_cp <SND\RobIII_cp>
2013-02-22 12:14:37 +0000

    Branched from $/fop2ddclient/0.2

---
+++ 0.3/Fop2DD/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Fop2DD.Core;
+
+namespace Fop2DD
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            using (var core = new DDCore())
+            {
+                core.Start();
+
+                Application.Run();
+
+                core.Stop();
+            }
+        }
+    }
+}
