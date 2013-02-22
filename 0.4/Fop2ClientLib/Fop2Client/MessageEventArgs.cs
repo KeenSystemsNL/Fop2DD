266a39f624c0c698580742ba8539b1bb293228ef
RobIII_cp <SND\RobIII_cp>
2013-02-22 18:37:11 +0000

    Branched from $/fop2ddclient/0.3

---
+++ 0.4/Fop2ClientLib/Fop2Client/MessageEventArgs.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Fop2ClientLib
+{
+    /// <summary>
+    /// Base class for MessageReceived/Sent events
+    /// </summary>
+    public abstract class MessageEventArgs : EventArgs
+    {
+    }
+
+    /// <summary>
+    /// Provides data for the <see cref="IFop2Client.MessageReceived"/> event.
+    /// </summary>
+    public class MessageReceivedEventArgs : MessageEventArgs
+    {
+        /// <summary>
+        /// Gets the message that was received by a client.
+        /// </summary>
+        public Fop2Message Message { get; private set; }
+
+        /// <summary>
+        /// Initializes a new MessageReceivedEventArgs.
+        /// </summary>
+        /// <param name="message">Message that was received.</param>
+        internal MessageReceivedEventArgs(Fop2Message message)
+        {
+            this.Message = message;
+        }
+    }
+
+    /// <summary>
+    /// Provides data for the <see cref="IFop2Client.MessageSent"/> event.
+    /// </summary>
+    public class MessageSentEventArgs : MessageEventArgs
+    {
+        /// <summary>
+        /// Gets the message that was sent by a client.
+        /// </summary>
+        public string Message { get; private set; }
+
+        /// <summary>
+        /// Initializes a new MessageSentEventArgs.
+        /// </summary>
+        /// <param name="data">Data associated with the MessageSentEventArgs.</param>
+        internal MessageSentEventArgs(string data)
+        {
+            this.Message = data;
+        }
+    }
+}
